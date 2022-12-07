c5a155f0b085aeb7f48afae9873ce485ee86125f
Tim Ho <github.timho@gmail.com>
2022-12-06 20:17:11 -0700

    add form validation, form reset, update snackbar css

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
-import { Component, OnDestroy, OnInit } from '@angular/core';
-import {FormControl, FormGroup} from '@angular/forms';
+import { Component, OnDestroy, OnInit, ViewChild } from '@angular/core';
+import {FormControl, FormGroup, NgForm, Validators} from '@angular/forms';
 import { Subscription } from 'rxjs';
 import { State } from './models/state';
 import { HttpServices } from './services/http.service';
@@ -21,6 +21,9 @@ export class AppComponent implements OnInit, OnDestroy {
   public stateName: string[] = [];
   public stateAbbreviation: string[] = [];
 
+  // Angular feature allow to manipulate html tag from the component
+  @ViewChild('myForm') myForm!: NgForm;
+
   // inject http service into app.component as dependency 
   constructor(
     private httpService: HttpServices,
@@ -29,11 +32,11 @@ export class AppComponent implements OnInit, OnDestroy {
     // *it is not recommend to initilizes any variables or form within the constructor in Angular.
     // sometime it is neccessary to do so in this case for FormGroup.
     this.profileForm = new FormGroup({
-      name: new FormControl(''),
-      email: new FormControl(''),
-      password: new FormControl(''),
-      occupation: new FormControl(''),
-      state: new FormControl('')
+      name: new FormControl('', Validators.required),
+      email: new FormControl('', Validators.required),
+      password: new FormControl('', Validators.required),
+      occupation: new FormControl('', Validators.required),
+      state: new FormControl('', Validators.required)
     });
   }
   
@@ -53,18 +56,17 @@ export class AppComponent implements OnInit, OnDestroy {
     })
   }
 
-  // this method using angular service to send user submited form to 'https://frontend-take-home.fetchrewards.com/form'
+  // Using angular service to send user submited form to 'https://frontend-take-home.fetchrewards.com/form'
   // after submited form sended to 'https://frontend-take-home.fetchrewards.com/form', this subscription will be destroy to prevent memory leak or any other problem happen later on.
   // this is another best pratice when using subscription.
   public onSubmit(): void {
     this.subscription  = this.httpService.submitForm(this.profileForm.value).subscribe()
-  }
 
-  // this is an Angular method allow subscription to unsubscribe from Observable.
-  ngOnDestroy(): void {
-    this.subscription.unsubscribe()
+    // After user submit their info, the form will reset to normal allow user to enter another submit. 
+    this.profileForm.reset()
+    this.myForm.resetForm();
   }
-
+  
   // Pop-up bar on top of the application after the user submitted their information
   // snack-bar duration last for 2s then disappear  
   public openSnackBar() {
@@ -72,5 +74,10 @@ export class AppComponent implements OnInit, OnDestroy {
       duration: 2000,
       verticalPosition: 'top'
     });
+  }
+
+  // Angular method allow subscription to unsubscribe from Observable.
+  ngOnDestroy(): void {
+    this.subscription.unsubscribe()
   }
 }
--- src/app/snack-bar/snack-bar.component.ts
+++ src/app/snack-bar/snack-bar.component.ts
@@ -2,10 +2,15 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-snack-bar',
-  template: '<span class="snack">Submit success!!!</span>',
+  template: '<span class="snack">Submit Successful !!!</span>',
   styles: [`
     .snack {
-      color: hotpink;
+      display: flex;
+      justify-content: center;
+      font-size: 1.1rem;
+      font-weight: 400;
+      line-height: 1.3rem;
+      color: hotpink
     }
 `]
 })
